3badfe32d391cde3af32e0a14641dea1a4dae8d5
Dito <ammarditoshafaat2001@gmail.com>
2022-12-15 04:56:26 +0700

    anjay gile

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template, make_response, url_f
 import json
 import jwt # Perlu install pip3 install PyJWT diawal
 import datetime
+import urllib.request, json
 from functools import wraps
 from flask_mysqldb import MySQL
 
@@ -16,28 +17,45 @@ table = 'house'
 
 app.config['SECRET_KEY'] ='needbucin'
 storage = []
-
+headerTable = ['status','price','bed','bath','acre_lot','full_address','street','city','state','zip_code','house_size','sold_date','crimeRate']
 # Define what the app does
 
 # Homepage
 @app.get("/")
 def landing():
-    return jsonify('Home Page API House Recommendation Considering Crime Rate')
+    return jsonify('Welcome to API House Recommendation with Considering Crime Rate')
 
 # Core-API -> Pemanfaat API dari teman sekelompok : Aufa Fauqi Ardhiqi(18220096)   
 @app.route('/core-api',methods=['GET','POST'])
 # @token_required
 def coreAPI():
+    url = 'http://34.101.40.1/data/crimesRate?sort=desc'
+    response = urllib.request.urlopen(url)
+    dataAPI = response.read()
+    crimesRateData = json.loads(dataAPI)
+    lenOfHeader = len(headerTable)
     cur = mysql.connection.cursor()
-    cleanData = []
-    form = request.form
-    state = form['state']
-    city = form['city']
-    return jsonify('Home Page API House Recommendation Considering Crime Rate')
+    stateList = ['New York','New Jersey']
+    crimeRate = ['65','69']
+    cleanData = ()
+    # form = request.form
+    # state = form['state']
+    # city = form['city']
+    # print(crimesRateData)
+    for i in range (0,(len(crimesRateData) - 1)):
+        query = f'''select *, "{crimesRateData[i]['crimeRate']}" as crimeRate from {table} where state = "{crimesRateData[i]['state']}" limit 1 '''
+        print(query)
+        cur.execute(query)
+        data = cur.fetchall()
+        cleanData += data
+    print(cleanData)
+    lenData = len(cleanData)
+    print(len(crimesRateData))
+    return render_template('coreAPI.html',headerTable=headerTable,lenOfHeader=lenOfHeader,lenData=lenData,data=cleanData)
 
 # Penyedia API untuk teman sekelompok dari saya Ammardito Shafaat (18220074)
 @app.get("/siti")
-def panggil_siti ():
+def panggil_siti():
     stateList = []
     cur = mysql.connection.cursor()
     city = request.args.get("city")
