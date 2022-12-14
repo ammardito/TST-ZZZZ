6549c9c811dc421258b06b49865eae3112f2d80f
Dito <ammarditoshafaat2001@gmail.com>
2022-12-15 01:59:31 +0700

    baru mulai

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import jwt # Perlu install pip3 install PyJWT diawal
 import datetime
 from functools import wraps
 from flask_mysqldb import MySQL
-from user import *
+
 # Intitialise the app
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
@@ -17,14 +17,31 @@ table = 'house'
 app.config['SECRET_KEY'] ='needbucin'
 storage = []
 
-# City -> State
+# Define what the app does
+
+# Homepage
+@app.get("/")
+def landing():
+    return jsonify('Home Page API House Recommendation Considering Crime Rate')
+
+# Core-API -> Pemanfaat API dari teman sekelompok : Aufa Fauqi Ardhiqi(18220096)   
+@app.route('/core-api',methods=['GET','POST'])
+# @token_required
+def coreAPI():
+
+    return jsonify('Home Page API House Recommendation Considering Crime Rate')
+
+# Penyedia API untuk teman sekelompok dari saya Ammardito Shafaat (18220074)
 @app.get("/siti")
-def panggil_sity():
+def panggil_siti ():
+    stateList = []
     cur = mysql.connection.cursor()
     city = request.args.get("city")
     if not city :
-        return jsonify("City tidak ditemukan")
-    # query
-    # cur.execute(query)
+        return jsonify({"City tidak ditemukan"})
+    query = f'SELECT DISTINCT state FROM {table} WHERE city = "{city}"'
+    cur.execute(query)
     data = cur.fetchall()
-    return jsonify(data)
+    for d in data:
+        stateList.append(d[0])
+    return jsonify(stateList)
